6ddd890b69fb9373a1fb81da8ffd2f14aa66138d
sld1993 <1624950137@qq.com>
2019-07-12 15:17:29 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/test/weather/dweather/MyApplication.java
@@ -0,0 +1,13 @@
+package com.test.weather.dweather;
+
+import android.app.Application;
+
+import org.litepal.LitePal;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LitePal.initialize(this);
+    }
+}
